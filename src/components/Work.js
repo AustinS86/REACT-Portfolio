beaf538383cf28711131eeed3e2d36edc7505613
Austin Stancil <117120566+AustinS86@users.noreply.github.com>
2023-04-23 18:03:17 -0400

    added motion blue to portfolio section

--- src/components/Work.js
+++ src/components/Work.js
@@ -2,6 +2,7 @@
 import React from "react";
 import Project from "./Project";
 import Container from "./Container";
+import { motion } from "framer-motion";
 import CodeScreen from "../assets/images/pexels-pixabay-256502.jpg";
 import Totoro from "../assets/images/totoro-bw.png";
 import Peard from "../assets/images/spaghetti-1932466_1920 (1).jpg";
@@ -10,13 +11,23 @@ import Weather from "../assets/images/weather-dashboard-screenshot.jpg";
 import TechBlog from "../assets/images/Developer-Tech-Blog.jpg";
 
 const Work = () => {
+  const varients = {
+    hidden: { opacity: 0, x: -200 },
+    visible: { opacity: 1, x: 0 },
+  };
   return (
     <section id="work" className="mb-0 ">
       <Container>
         <h1 className="text-4xl mb-4 font-bold text-white">
           Projects & Challenges
         </h1>
-        <div className="flex flex-wrap text-white space-x-4 space-y-4 lg:space-x-20 lg:space-y-0">
+        <motion.div
+          className="flex flex-wrap text-white space-x-4 space-y-4 lg:space-x-20 lg:space-y-0"
+          initial="hidden"
+          animate="visible"
+          variants={varients}
+          transition={{ duration: 0.5 }}
+        >
           <Project
             title="Challenge-Accessibility"
             imgSrc={CodeScreen}
@@ -53,7 +64,7 @@ const Work = () => {
             alt="Tech Blog Home Page"
             url="https://github.com/AustinS86/Developer-Tech-Blog"
           />
-        </div>
+        </motion.div>
       </Container>
     </section>
   );
